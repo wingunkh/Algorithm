c0889f1474e579c5be09bce28726fdd80cbfa46d
wingunkh <99gusrms@naver.com>
2023-01-04 00:37:15 +0900

    ch03 검색 알고리즘

---
+++ ch03 검색 알고리즘/a_선형 검색 알고리즘.py
@@ -0,0 +1,26 @@
+# 선형 검색 알고리즘
+
+from typing import Any, Sequence
+
+def seq_search(a: Sequence, key: Any) -> int:
+    for i in range(len(a)):
+        if a[i] == key:
+            return 1 # 검색 성공 (검색할 값과 같은 원소를 찾은 경우)
+    return -1 # 검색 실패 (검색할 값을 찾지 못하고 배열의 맨 끝을 지나간 경우)
+
+if __name__ == "__main__":
+    num = int(input("원소 수를 입력하세요.: "))
+    x = [None] * num
+
+    for i in range(num):
+        x[i] = int(input(f"x[{i}]: "))
+
+
+    ky = int(input("검색할 값을 입력하세요.: "))
+
+    idx = seq_search(x, ky)
+
+    if idx == -1:
+        print("검색값을 갖는 원소가 존재하지 않습니다.")
+    else:
+        print(f"검색값은 x[{idx}]에 있습니다.")
