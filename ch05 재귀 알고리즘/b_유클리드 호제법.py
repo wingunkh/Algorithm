4fb5c0542fa82acbb0d4ada582d6fb6ca1fef56d
wingunkh <99gusrms@naver.com>
2023-01-15 16:38:11 +0900

    ch05 재귀 알고리즘

---
+++ ch05 재귀 알고리즘/b_유클리드 호제법.py
@@ -0,0 +1,15 @@
+# 유클리드 호제법으로 최대공약수(GCD) 구하기
+
+def gcd(x: int, y: int) -> int:
+    if x % y == 0:
+        return y
+    else:
+        return gcd(y, x % y)
+
+if __name__ == "__main__":
+    print("두 정숫값의 최대 공약수를 구합니다.")
+    x = int(input("첫번째 정숫값을 입력하세요.: "))
+    y = int(input("두번째 정숫값을 입력하세요.: "))
+
+    print(f"두 정숫값의 최대 공약수는 {gcd(x, y)}입니다.")
+    
