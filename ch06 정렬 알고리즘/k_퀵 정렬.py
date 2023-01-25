d58ffe79e6a19ccf50194da683a58e5c3f08f194
wingunkh <99gusrms@naver.com>
2023-01-25 18:04:54 +0900

    ch06 정렬 알고리즘

---
+++ ch06 정렬 알고리즘/k_퀵 정렬.py
@@ -0,0 +1,39 @@
+# 퀵 정렬 알고리즘 구현하기
+
+from typing import MutableSequence
+
+def qsort(a: MutableSequence, left: int, right: int) -> None:
+    n = len(a)
+    pl = left
+    pr = right
+    x = a[(left + right) // 2] # 피벗
+
+    print(f"a[{left}] ~ a[{right}]:", *a[left : right + 1])
+
+    while pl <= pr:
+        while a[pl] < x: pl += 1
+        while a[pr] > x: pr -= 1
+        if pl <= pr:
+            a[pl], a[pr] = a[pr], a[pl]
+            pl += 1
+            pr -= 1
+
+    if left < pr : qsort(a, left, pr)
+    if pl < right: qsort(a, pl, right)
+
+def quick_sort(a: MutableSequence) -> None:
+    qsort(a, 0, len(a) - 1)
+
+if __name__ == "__main__":
+    print("퀵 정렬을 수행합니다.")
+    num = int(input("원소 수를 입력하세요.: "))
+    x = [None] * num
+
+    for i in range(num):
+        x[i] = int(input(f"x[{i}]: "))
+
+    quick_sort(x)
+
+    print("오름차순으로 정렬했습니다.")
+    for i in range(num):
+        print(f"x[{i}] = {x[i]}")
