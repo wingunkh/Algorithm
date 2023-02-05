4b0e892e22c9a6cf80cd406ce9eea2cec515cc0d
wingunkh <99gusrms@naver.com>
2023-02-05 14:14:16 +0900

    ch08 리스트

--- ch08 리스트/a_연결리스트.py
+++ ch08 리스트/a_연결리스트.py
@@ -42,7 +42,84 @@ class LinkedList:
             self.add_first(data)
         else:
             ptr = self.head
+            
             while ptr is not None:
                 ptr = ptr.next
-            ptr.next = ptr.current = None(data, None)
+            ptr.next = ptr.current = Node(data, None)
             self.no += 1
+
+    def remove_first(self) -> None:
+        if self.head is not None:
+            self.head = self.current = self.head.next
+        self -= no
+
+    def remove_last(self) -> None:
+        if self.head is not None:
+            if self.next is None:
+                self.remove_first()
+            else:
+                ptr = self.head # 스캔 중인 노드
+                pre = self.head # 스캔 중인 노드의 앞쪽 노드
+
+                while ptr.next is not None:
+                    pre = ptr
+                    ptr = ptr.next
+                pre.next = None
+                self.current = pre
+                self.no -= 1
+
+    def remove(self, p: Node) -> None:
+        if self.head is not None:
+            if p is self.head:
+                remove_first()
+            else:
+                ptr = self.head
+                
+                while ptr.next is not p:
+                    ptr = ptr.next
+                    if ptr is None:
+                        return
+                ptr.next = p.next
+                self.current = ptr
+                self.no -= 1
+
+    def remove_current_node(self) -> None:
+        self.remove(self.current)
+
+    def clear(self) -> None:
+        while self.head is not None:
+            self.remove_first()
+        self.current = None
+        self.no = 0
+
+    def next(self) -> bool:
+        if self.current is None or self.current.next is None:
+            return False
+        self.current = self.current.next
+        return True
+
+    def print_current_node(self) -> None:
+        if self.current is None:
+            print("주목 노드가 존재하지 않습니다.")
+        else:
+            print(self.current.data)
+
+    def print(self) -> None:
+        ptr = self.head
+
+        while ptr is not None:
+            print(ptr.data)
+            ptr = ptr.next
+
+    # 클래스 LinkedList가 이터러블하도록 iter() 함수와 next() 함수를 구현한다.
+    def __iter__(self): # __next__() 함수를 갖는 이터레이터(반복자)를 반환하는 __iter()__ 함수 구현
+        self.current = self.head
+        return self
+
+    def __next__(self): # 다음 원소를 꺼내 반환하며 반환하는 원소가 없으면 StopIteration 예외처리를 내보내는 __next()__ 함수 구현
+        if self.current is None:
+            raise StopIteration
+        else:
+            data = self.current.data
+            self.current = self.current.next
+            return data
