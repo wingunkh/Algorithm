3668f262595b1412fff2105c2bc3759e4e4dd421
wingunkh <99gusrms@naver.com>
2023-02-11 16:45:27 +0900

    ch09 트리

--- ch09 트리/a.이진검색트리.py
+++ ch09 트리/a.이진검색트리.py
@@ -47,3 +47,55 @@ class BinarySearchTree:
             return True
         else:
             return add_node(self.root, key, value)
+
+    def remove(self, key: Any) -> bool:
+        p = self.root # 스캔 중인 노드
+        parent = None # 스캔 중인 노드의 부모 노드
+        is_left_child = True # p는 parent의 왼쪽 자식 노드인지 확인
+
+        while True: # 삭제할 키를 검색
+            if p is None:
+                return False
+
+            if key == p.key:
+                break
+            else:
+                parent = p
+                if key < p.key:
+                    is_left_child = True
+                    p = p.left
+                else:
+                    is_left_child = False
+                    p = p.left
+
+        if p.left is None: # p에 왼쪽 자식이 없을 때
+            if p is self.root:
+                self.root = p.right
+            elif is_left_child:
+                parent.left = p.right
+            else:
+                parent.right = p.right
+        elif p.right is None: # p에 오른쪽 자식이 없을 때
+            if p is self.root:
+                self.root = p.left
+            elif is_left_child:
+                parent.left = p.left
+            else:
+                parent.right = p.left
+                
+        else:
+            parent = p
+            left = p.left
+            is_left_child = True
+            while left.right is not None:
+                parent = left
+                left = left.right
+                is_left_child = False
+
+            p.key = left.key
+            p.value = left.value
+            if is_left_child:
+                parent.left = left.left
+            else:
+                parent.right = left.left
+            return True
