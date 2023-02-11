cfebff0619d196339bffd52e9ec777c959d457fa
wingunkh <99gusrms@naver.com>
2023-02-11 21:37:56 +0900

    ch08 리스트

--- ch08 리스트/d_원형 이중 연결 리스트 활용.py
+++ ch08 리스트/d_원형 이중 연결 리스트 활용.py
@@ -3,17 +3,17 @@
 from enum import Enum
 from c_원형이중연결리스트 import DoubleLinkedList
 
-Menu = Enum('Menu', ['머리에노드삽입', '꼬리에노드삽입', '주목노드바로뒤삽입',
-                     '머리노드삭제', '꼬리노드삭제', '주목노드출력',
-                     '주목노드이동', '주목노드역순이동', '주목노드삭제',
-                     '모든노드삭제', '검색', '멤버십판단', '모든노드출력',
-                     '모든노드역순출력', '모든노드스캔', '모든노드역순스캔', '종료'])
+Menu = Enum("Menu", ["머리에노드삽입", "꼬리에노드삽입", "주목노드바로뒤삽입",
+                     "머리노드삭제", "꼬리노드삭제", "주목노드출력",
+                     "주목노드이동", "주목노드역순이동", "주목노드삭제",
+                     "모든노드삭제", "검색", "멤버십판단", "모든노드출력",
+                     "모든노드역순출력", "모든노드스캔", "모든노드역순스캔", "종료"])
 
 def select_Menu() -> Menu:
-    s = [f'({m.value}){m.name}' for m in Menu]
+    s = [f"({m.value}){m.name}" for m in Menu]
     while True:
         print(*s, sep = '  ', end='')
-        n = int(input(': '))
+        n = int(input(": "))
         if 1 <= n <= len(Menu):
             return Menu(n)
 
@@ -23,13 +23,13 @@ while True:
     menu = select_Menu()
 
     if menu == Menu.머리에노드삽입:
-        lst.add_first(int(input('머리 노드에 넣을 값을 입력하세요.: ')))
+        lst.add_first(int(input("머리 노드에 넣을 값을 입력하세요.: ")))
 
     elif menu == Menu.꼬리에노드삽입:
-        lst.add_last(int(input('꼬리 노드에 넣을 값을 입력하세요.: ')))
+        lst.add_last(int(input("꼬리 노드에 넣을 값을 입력하세요.: ")))
 
     elif menu == Menu.주목노드바로뒤삽입:
-        lst.add(int(input('주목 노드 바로 뒤에 넣을 값을 입력하세요 : ')))
+        lst.add(int(input("주목 노드 바로 뒤에 넣을 값을 입력하세요 : ")))
 
     elif menu == Menu.머리노드삭제:
         lst.remove_first()
@@ -53,15 +53,15 @@ while True:
         lst.clear()
 
     elif menu == Menu.검색:
-        pos = lst.search(int(input('검색할 값을 입력하세요.: ')))
+        pos = lst.search(int(input("검색할 값을 입력하세요.: ")))
         if pos >= 0:
-            print(f'그 값의 데이터는 {pos + 1}번째에 있습니다.')
+            print(f"그 값의 데이터는 {pos + 1}번째에 있습니다.")
         else:
-            print('해당 데이터가 없습니다.')
+            print("해당 데이터가 없습니다.")
 
     elif menu == Menu.멤버십판단:
-        print('그 값의 데이터는 포함되어'
-              +(' 있습니다.' if int(input('판단할 값을 입력하세요.: ')) in lst else ' 있지 않습니다.'))
+        print("그 값의 데이터는 포함되어"
+              +(" 있습니다." if int(input("판단할 값을 입력하세요.: ")) in lst else " 있지 않습니다."))
 
     elif menu == Menu.모든노드출력:
         lst.print()
