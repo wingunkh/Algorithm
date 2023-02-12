b2128843fd119bf4234463b9c4e2b22bbb363f0a
wingunkh <99gusrms@naver.com>
2023-02-12 17:38:57 +0900

    ch01 알고리즘 기초

--- ch01 알고리즘 기초/l_별찍기 이등변 삼각형.py
+++ ch01 알고리즘 기초/l_별찍기 이등변 삼각형.py
@@ -12,7 +12,7 @@ print("오른쪽 아래가 직각인 이등변 삼각형을 출력합니다.")
 n = int(input("짧은 변의 길이를 입력하세요.: "))
 
 for i in range(n):
-    for _ in range(n-i-1):
+    for _ in range(n-1-i):
         print(' ', end='')
     for _ in range(i+1):
         print('*', end='')
